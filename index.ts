95464697b514d71fa872ca8266e2646a13496fd2
Mehraz <mehrazhossain54@gmail.com>
2023-08-30 09:00:09 +0600

    First Query With Prisma Client ( Create And Read )

--- index.ts
+++ index.ts
@@ -6,13 +6,13 @@ async function main() {
   const getAllUsers = await prisma.user.findMany();
   console.log(getAllUsers);
 
-  //   const postUser = await prisma.user.create({
-  //     data: {
-  //       email: "mehraz@gmail.com",
-  //       name: "Mehraz Hossain",
-  //     },
-  //   });
-  //   console.log(postUser);
+  const postUser = await prisma.user.create({
+    data: {
+      email: "mehraz@gmail.com",
+      name: "Mehraz Hossain",
+    },
+  });
+  console.log(postUser);
 }
 
 main();
